038b1fef2f94d0de3ff00d2738338abe911910aa
mdYeasin-cmd <yeasin200011@gmail.com>
2023-01-22 19:12:52 +0600

    Navbar component added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,66 +1,78 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import logo from './../../assets/Logo/logo.png';
+import './Navbar.css';
 
 const Navbar = () => {
+
+    const activeNavItem = () => {
+
+        return ({ isActive }: { isActive: boolean }) : string | undefined => isActive ?
+            'nav-item-active' :
+            'nav-item';
+
+    }
+
     return (
         <div>
 
             {/* Navbar */}
-            <nav className="flex justify-between bg-gray-900 text-white w-screen">
-                <div className="px-5 xl:px-12 py-6 flex w-full items-center">
-                    <a className="text-3xl font-bold font-heading" href="#">
-                        <img className="h-9" src="logo.png" alt="logo" />
-                        {/* Logo Here. */}
-                    </a>
-                    {/* <!-- Nav Links --> */}
+            <nav className="flex justify-between text-[#34526e] w-screen border-b-4">
+                <div className="px-5 xl:px-12 py-2 flex w-full items-center">
+                    {/* Logo Here */}
+                    <Link className="flex items-center text-3xl font-bold font-heading" to="/">
+                        <img className="h-16" src={logo} alt="logo" />
+                        <h1 className="ml-[-55px] font-semibold">Skills House</h1>
+                    </Link>
+                    {/* Nav Links */}
                     <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-                        <li><Link className="hover:text-gray-200" to="/">Home</Link></li>
-                        <li><Link className="hover:text-gray-200" to="/courses">Courses</Link></li>
-                        <li><Link className="hover:text-gray-200" to="/blog">Blog</Link></li>
-                        <li><Link className="hover:text-gray-200" to="/contact">Contact</Link></li>
+                        <li><NavLink className={activeNavItem()} to="/">Home</NavLink></li>
+                        <li><NavLink className={activeNavItem()} to="/courses">Courses</NavLink></li>
+                        <li><NavLink className={activeNavItem()} to="/blog">Blog</NavLink></li>
+                        <li><NavLink className={activeNavItem()} to="/contact">Contact</NavLink></li>
                     </ul>
-                    {/* <!-- Header Icons --> */}
-                    <div className="hidden xl:flex items-center space-x-5 items-center">
-                        <a className="hover:text-gray-200" href="#">
+                    {/* Header Icons */}
+                    <div className="hidden xl:flex items-center space-x-5">
+                        <Link className="nav-icon" to="/">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                             </svg>
-                        </a>
-                        <a className="flex items-center hover:text-gray-200" href="#">
+                        </Link>
+                        <Link className="flex items-center nav-icon" to="/">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                             </svg>
                             <span className="flex absolute -mt-5 ml-4">
                                 <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
                                 <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
                                 </span>
                             </span>
-                        </a>
-                        {/* <!-- Sign In / Register      --> */}
-                        <a className="flex items-center hover:text-gray-200" href="#">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </Link>
+                        {/* Sign In / Register */}
+                        <Link className="flex items-center nav-icon" to="/">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 nav-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
-                        </a>
+                        </Link>
 
                     </div>
                 </div>
-                {/* <!-- Responsive navbar --> */}
-                <a className="xl:hidden flex mr-6 items-center" href="#">
+                {/* Responsive navbar */}
+                <Link className="xl:hidden flex mr-6 items-center" to="/">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                     </svg>
                     <span className="flex absolute -mt-5 ml-4">
                         <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
                         <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
                         </span>
                     </span>
-                </a>
-                <a className="navbar-burger self-center mr-12 xl:hidden" href="#">
+                </Link>
+                <Link className="navbar-burger self-center mr-12 xl:hidden" to="/">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
-                </a>
+                </Link>
             </nav>
 
         </div>
